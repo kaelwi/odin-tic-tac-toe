b144ebee6159f361b0e84bebaed1ba2e3e081c49
Karoline Wild <karoline.elisabeth.wild@seznam.cz>
2022-09-09 12:37:00 +0200

    Render board, set mark, add mark to div when clicked

--- script.js
+++ script.js
@@ -12,24 +12,41 @@ const game = (() => {
   const renderBoard = () => {
     board.forEach(element => {
       let div = document.createElement('div');
-      div.textContent = element;
       container.appendChild(div);
     });
   }
 
-  return { renderBoard };
+  return { renderBoard, player, computer };
 })();
 
 // gameboard.renderBoard();
 
 const displayController = (() => {
+
+  let chosen = false;
+
   document.getElementById('start').addEventListener('click', () => {
     game.renderBoard();
   })
 
   document.querySelectorAll('#button-container > button').forEach((button) => {
-    button.addEventListener('click', () => {
-      
+    button.addEventListener('click', e => {
+      if (!chosen) {
+        let mark = e.target.dataset.mark;
+        game.player = playerFactory(mark);
+        game.computer = playerFactory(mark == 'x' ? 'o' : 'x');
+        chosen = true;
+      }
     })
   })
+
+  container.addEventListener('click', e => {
+    if (typeof game.player !== 'undefined' && e.target.children.length == 0) {
+      let heading = document.createElement('h1');
+      heading.textContent = game.player.mark;
+      e.target.appendChild(heading);
+    }
+  })
 })();
+
+game.renderBoard();
